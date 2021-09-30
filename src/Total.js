d79e88acf0dc354d706c60a88275648a7ac72e0a
catalinpit <catalinpit@gmail.com>
2021-09-30 16:53:00 +0300

    Re-structured the app a bit

---
+++ src/Total.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Total = ({ exercises }) => {
+    return (
+        <p>
+        Number of exercises {exercises}
+        </p>
+    )
+};
+
+export default Total;
