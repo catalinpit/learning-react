e7ae98d19c0df61e940f5da00493e68bc5a846cd
catalinpit <catalinpit@gmail.com>
2021-10-07 18:38:36 +0300

    Phone exercise part1 finished

---
+++ phonebook/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    {
+      name: 'Arto Hellas'
+    }
+  ]);
+
+  const [ newName, setNewName ] = useState('');
+
+  const addPerson = (e) => {
+    e.preventDefault();
+
+    const newPerson = {
+      name: newName
+    };
+
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+  };
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  return (
+    <>
+      <h2>Phone book</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+    </>
+  )
+};
+
+export default App;
