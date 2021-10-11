aa8884c706c3c60aecce16910630efbce836f70a
catalinpit <catalinpit@gmail.com>
2021-10-11 17:05:23 +0300

    Using the useEffect

--- notes/src/index.js
+++ notes/src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import axios from 'axios';
-
-axios
-  .get('http://localhost:3001/notes')
-  .then((response) => {
-    const res = response.data;
-
-    console.log('result', res);
-  });
 
 ReactDOM.render(
   <React.StrictMode>
