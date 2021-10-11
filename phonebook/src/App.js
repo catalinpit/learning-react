a44d4d5d0a220d71337678cfe606a2272ad22c75
catalinpit <catalinpit@gmail.com>
2021-10-11 08:47:36 +0300

    Moved the persons array

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,13 +1,7 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+const App = (props) => {
+  const [persons, setPersons] = useState(props.people)
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [search, setNewSearch] = useState('');
