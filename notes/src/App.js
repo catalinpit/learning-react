adc2b9e6d8a0ff3145d690568a4f08fb7affad06
catalinpit <catalinpit@gmail.com>
2021-10-23 09:17:02 +0300

    Added a notification component

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Note from './components/Note';
+import Notification from './components/Notification';
 import noteService from '../src/services/notes';
 import './index.css';
 
@@ -7,6 +8,7 @@ const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('some error happened!');
 
   const addNote = (e) => {
     e.preventDefault();
@@ -44,7 +46,7 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(err => {
-        alert(
+        setErrorMessage(
           `the note '${note.content}' was already deleted from the server`
         )
         setNotes(notes.filter(note => note.id !== id))
@@ -62,6 +64,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
           <button onClick={() => setShowAll(!showAll)}>
               show {showAll ? 'important' : 'all'}
